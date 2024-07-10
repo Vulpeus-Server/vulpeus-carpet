d705cc0537fb5792a5643d14ecedcded32ab1b0a
topi-banana <support@topi-banana.com>
2024-07-11 06:09:35 +0900

    not need yet

--- src/main/java/com/vulpeus/VulpeusCarpet/VulpeusCarpetSettings.java
+++ src/main/java/com/vulpeus/VulpeusCarpet/VulpeusCarpetSettings.java
@@ -7,8 +7,6 @@ import carpet.settings.Rule;
 public class VulpeusCarpetSettings {
 
   private static final String VULPEUS = "vulpeus";
-  private static final String LOGGER = "logger";
-  private static final String PROTOCOL = "protocol";
 
   @Rule(
       desc = "Enable /view command to changes the view distance of the server without op.",
