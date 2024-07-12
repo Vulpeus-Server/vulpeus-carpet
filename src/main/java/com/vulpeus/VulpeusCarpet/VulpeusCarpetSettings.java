f9721fdf0ac6697878bbd83a232987352b51e387
topi-banana <support@topi-banana.com>
2024-07-12 09:07:46 +0900

    changed the order to match README.md

--- src/main/java/com/vulpeus/VulpeusCarpet/VulpeusCarpetSettings.java
+++ src/main/java/com/vulpeus/VulpeusCarpet/VulpeusCarpetSettings.java
@@ -9,11 +9,6 @@ public class VulpeusCarpetSettings {
   private static final String VULPEUS = "vulpeus";
 
   @Rule(
-      desc = "Enable `/view` command to change viewDistance of carpet.",
-      category = {SURVIVAL, COMMAND, VULPEUS})
-  public static String commandView = "ops";
-
-  @Rule(
       desc = "Enable `/hat` command. Ported from essential addons.",
       category = {SURVIVAL, COMMAND, VULPEUS})
   public static String commandHat = "ops";
@@ -24,7 +19,14 @@ public class VulpeusCarpetSettings {
   public static String commandSit = "ops";
 
   @Rule(
-      desc = "Set the OP level granted when you join the server. can be re-granted by rejoining and can override `op-permission-level` in the server.properties.",
+      desc = "Enable `/view` command to change viewDistance of carpet.",
+      category = {SURVIVAL, COMMAND, VULPEUS})
+  public static String commandView = "ops";
+
+  @Rule(
+      desc =
+          "Set the OP level granted when you join the server. can be re-granted by rejoining and"
+              + " can override `op-permission-level` in the server.properties.",
       category = {VULPEUS},
       options = {"0", "1", "2", "3", "4"})
   public static int defaultOpLevel = 0;
