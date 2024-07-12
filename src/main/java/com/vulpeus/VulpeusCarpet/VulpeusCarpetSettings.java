c8a838d7fa5310987ea6342164273e34c3d077ae
topi-banana <support@topi-banana.com>
2024-07-12 09:05:35 +0900

    change description. written by rs256

--- src/main/java/com/vulpeus/VulpeusCarpet/VulpeusCarpetSettings.java
+++ src/main/java/com/vulpeus/VulpeusCarpet/VulpeusCarpetSettings.java
@@ -9,33 +9,33 @@ public class VulpeusCarpetSettings {
   private static final String VULPEUS = "vulpeus";
 
   @Rule(
-      desc = "Enable /view command to changes the view distance of the server without op.",
+      desc = "Enable `/view` command to change viewDistance of carpet.",
       category = {SURVIVAL, COMMAND, VULPEUS})
   public static String commandView = "ops";
 
   @Rule(
-      desc = "Allows you to equip items to your head slot using /hat. Ported from EssentialAddons.",
+      desc = "Enable `/hat` command. Ported from essential addons.",
       category = {SURVIVAL, COMMAND, VULPEUS})
   public static String commandHat = "ops";
 
   @Rule(
-      desc = "Player can sit down when using /sit. Ported from PCA.",
+      desc = "Enable `/sit` command. Ported from PCA.",
       category = {SURVIVAL, COMMAND, VULPEUS})
   public static String commandSit = "ops";
 
   @Rule(
-      desc = "Set OP-permission when join to server.",
+      desc = "Set the OP level granted when you join the server. can be re-granted by rejoining and can override `op-permission-level` in the server.properties.",
       category = {VULPEUS},
       options = {"0", "1", "2", "3", "4"})
   public static int defaultOpLevel = 0;
 
   @Rule(
-      desc = "yeet ClassCastException crash.",
+      desc = "yeet the server crash caused by ClassCastException.",
       category = {VULPEUS})
   public static boolean disableCCECrash = false;
 
   @Rule(
-      desc = "yeet StackOverflowError crash.",
+      desc = "yeet the server crash caused by StackOverflowError.",
       category = {VULPEUS})
   public static boolean disableSOFCrash = false;
 
